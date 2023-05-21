f3462f193c03984144a58b79c7b49d247beaf4e8
Jairo Rodriguez <rodriguezcastrojairo13@gmail.com>
2023-05-20 23:28:41 -0600

    Ejercicio4

---
+++ Ejercicio4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Ejercicio4 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un primer numero entero");
+        int num1 = input.nextInt();
+        System.out.println("Ingrese un segundo numero entero");
+        int num2 = input.nextInt();
+
+        System.out.println("La suma de estos numeros es: " + (num1 + num2));
+        System.out.println("La resta de estos numeros es: " + (num1 - num2));
+        System.out.println("La multiplicacion de estos numeros es: " + (num1 * num2));
+        System.out.println("La division de estos numeros es: " + (num1 / num2));
+
+
+
+
+
+        
+        
+    }
+}
+
